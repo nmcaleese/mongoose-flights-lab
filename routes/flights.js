7b5aa9576ed78cff74f32bbdba9f5f2ce82bc1ff
Nathan <nmcaleese87@gmail.com>
2022-06-21 18:16:15 -0700

    adds new page html, styling, and functionality

--- routes/flights.js
+++ routes/flights.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 const flightsCtrl = require('../controllers/flights')
 
-
+//all routes here are prefixed with flights
 //routers
 router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
+
 
 module.exports = router;
