84d37af750ccf396c92e9df79ba7310d3ea96e82
Nathan <nmcaleese87@gmail.com>
2022-06-23 04:24:37 -0700

    adds default date functionality workaround

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,32 +1,31 @@
 // const { create } = require('../models/flight');
-const Flight = require('../models/flight');
+const Flight = require("../models/flight");
 
 module.exports = {
-    index,
-    new: newFlight,
-    create,
-}
+  index,
+  new: newFlight,
+  create,
+};
 
-function index(req, res){
-    Flight.find({}, function(err, flights){
-         res.render('flights/index', {title: 'All Flights', flights});
-    })
+function index(req, res) {
+  Flight.find({}, function (err, flights) {
+    res.render("flights/index", { title: "All Flights", flights });
+  });
 }
 
 function newFlight(req, res) {
-    res.render('flights/new', {title: 'Add Flight'});
+    let today = new Date()
+    let defaultDate = today.toISOString().slice(0,16)
+    res.render("flights/new", { title: "Add Flight" , defaultDate});
 }
 
 function create(req, res) {
-    // reformatting
-    req.body.flightNo = req.body.flightNo.replace(/\s/g, '')
-    //create new movie with Flights
-    let flight = new Flight(req.body);
-    flight.save(function(err){
-        if (err) return res.redirect('/flights/new')
-        console.log(flight);
-    })
-    console.log(flight)
-    // req.body.airport = 
-    res.redirect('/flights')
+  // reformatting
+  req.body.flightNo = req.body.flightNo.replace(/\s/g, "");
+  //create new  with Flights
+  let flight = new Flight(req.body);
+  flight.save(function(err) {
+    if (err) return res.redirect("/flights/new");
+    res.redirect("/flights");
+  });
 }
--- models/flight.js
+++ models/flight.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: {
-        type: String,
-        enum: ['American', 'Southwest', 'United'],
-        required: true,
-    },
-    airport: {
-        type: String,
-        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
-        required: true,
-        default: 'DEN'
-    },
-    flightNo: {
-        type: Number,
-        min: 10,
-        max: 9999,
-        required: true
-    },
-    departs: {
-        type: Date,
-        // default: 
-    },
-})
+  airline: {
+    type: String,
+    enum: ["American", "Southwest", "United"],
+    required: true,
+  },
+  airport: {
+    type: String,
+    enum: ["AUS", "DFW", "DEN", "LAX", "SAN"],
+    required: true,
+    default: "DEN",
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+    required: true,
+  },
+  departs: {
+    type: Date,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Flight', flightSchema);
+module.exports = mongoose.model("Flight", flightSchema);
