94242877779b2b6b86075254072f235b5ee433c2
Nathan <nmcaleese87@gmail.com>
2022-06-24 05:47:02 -0700

    adds dropdown menu with optional flight in new ticket form

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,22 +1,28 @@
-const Ticket = require('../models/ticket');
-const Flight = require('../models/flight');
+const Ticket = require("../models/ticket");
+const Flight = require("../models/flight");
 
 module.exports = {
-    new: newTicket,
-    create,
+  new: newTicket,
+  create,
+};
+
+function create(req, res) {
+  Ticket.create(req.body, function (err, ticket) {
+    console.log(ticket);
+    res.redirect("/tickets/new");
+  });
 }
 
-function create(req, res){
-    Ticket.create(req.body, function (err, ticket){
-        res.redirect('/tickets/new')
-    })
-}
+function newTicket(req, res) {
+  Ticket.find({}, function (err, tickets) {
+    Flight.find({}, function (err, flights) {
+      console.log(flights);
 
-function newTicket(req, res){
-    Ticket.find({}, function (err, tickets) {
-        res.render('tickets/new', {
-            title: 'Add Ticket', 
-            tickets
-        })
-    })
+      res.render("tickets/new", {
+        title: "Add Ticket",
+        tickets,
+        flights,
+      });
+    });
+  });
 }
